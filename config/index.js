680904b8db24d05b73f57b7dc30c05d97052612c
liquidliang <liquidliang@tencent.com>
2020-10-16 18:05:24 +0800

    错误示例

--- config/index.js
+++ config/index.js
@@ -1,70 +1,92 @@
-const config = {
-  projectName: 'taro-test',
-  date: '2020-10-15',
-  designWidth: 750,
-  deviceRatio: {
-    640: 2.34 / 2,
-    750: 1,
-    828: 1.81 / 2
-  },
-  sourceRoot: 'src',
-  outputRoot: 'dist',
-  plugins: [],
-  defineConstants: {
-  },
-  copy: {
-    patterns: [
-    ],
-    options: {
-    }
-  },
-  framework: 'vue3',
-  mini: {
-    postcss: {
-      pxtransform: {
-        enable: true,
-        config: {
+const path = require('path');
 
-        }
-      },
-      url: {
-        enable: true,
-        config: {
-          limit: 1024 // 设定转换尺寸上限
-        }
-      },
-      cssModules: {
-        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-        config: {
-          namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
-        }
-      }
-    }
-  },
-  h5: {
-    publicPath: '/',
-    staticDirectory: 'static',
-    postcss: {
-      autoprefixer: {
-        enable: true,
-        config: {
-        }
-      },
-      cssModules: {
-        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-        config: {
-          namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
-        }
-      }
-    }
-  }
-}
+const config = {
+    projectName: 'taro-test',
+    date: '2020-10-15',
+    designWidth: 750,
+    deviceRatio: {
+        640: 2.34 / 2,
+        750: 1,
+        828: 1.81 / 2,
+    },
+    sourceRoot: 'src',
+    outputRoot: 'dist',
+    plugins: [],
+    defineConstants: {},
+    alias: {
+        src: path.resolve(__dirname, '../src'),
+    },
+    copy: {
+        patterns: [],
+        options: {},
+    },
+    framework: 'vue3',
+    mini: {
+        postcss: {
+            pxtransform: {
+                enable: true,
+                config: {},
+            },
+            url: {
+                enable: true,
+                config: {
+                    limit: 1024, // 设定转换尺寸上限
+                },
+            },
+            cssModules: {
+                enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+                config: {
+                    namingPattern: 'module', // 转换模式，取值为 global/module
+                    generateScopedName: '[name]__[local]___[hash:base64:5]',
+                },
+            },
+        },
+    },
+    h5: {
+        publicPath: '/',
+        staticDirectory: 'static',
+        postcss: {
+            autoprefixer: {
+                enable: true,
+                config: {},
+            },
+            cssModules: {
+                enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+                config: {
+                    namingPattern: 'module', // 转换模式，取值为 global/module
+                    generateScopedName: '[name]__[local]___[hash:base64:5]',
+                },
+            },
+        },
+        esnextModules: ['taro-ui-vue3'],
+        webpackChain(chain) {
+            chain.resolve.alias
+                .set(
+                    '@tarojs/components$',
+                    path.resolve(
+                        __dirname,
+                        '..',
+                        'node_modules/@tarojs/components/dist-h5/vue3/index.js'
+                    )
+                )
+                .set(
+                    '@tarojs/components/dist/taro-components/taro-components.css',
+                    path.resolve(
+                        __dirname,
+                        '..',
+                        'node_modules/@tarojs/components/dist/taro-components/taro-components.css'
+                    )
+                );
+        },
+        router: {
+            mode: 'browser', // 或者是 'browser'
+        },
+    },
+};
 
 module.exports = function (merge) {
-  if (process.env.NODE_ENV === 'development') {
-    return merge({}, config, require('./dev'))
-  }
-  return merge({}, config, require('./prod'))
-}
+    if (process.env.NODE_ENV === 'development') {
+        return merge({}, config, require('./dev'));
+    }
+    return merge({}, config, require('./prod'));
+};
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,30 +1,47 @@
 <template>
-  <view class="index">
-    <NumberDisplay/>
-    <NumberSubmit/>
-  </view>
+  <AtAccordion
+    @click="onClick"
+    title='点这里报错'
+    :open="value1"
+  >
+    <AtList :hasBorder="false">
+      <AtListItem
+        title='标题文字'
+        thumb='https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png'
+      />
+      <AtListItem
+        title='标题文字'
+        note='描述信息'
+        thumb='http://img12.360buyimg.com/jdphoto/s72x72_jfs/t10660/330/203667368/1672/801735d7/59c85643N31e68303.png'
+      />
+    </AtList>
+  </AtAccordion>
 </template>
 
 <script>
-import NumberDisplay from '../../components/NumberDisplay.vue'
-import NumberSubmit from '../../components/NumberSubmit.vue'
-
+import { AtAccordion, AtList, AtListItem } from 'taro-ui-vue3'
 export default {
-  name: 'Index',
+  name: 'AccordionDemo',
   components: {
-    NumberDisplay,
-    NumberSubmit
-  }
+    AtAccordion,
+    AtList,
+    AtListItem
+  },
+  data() {
+    return {
+      value1: false
+    }
+  },
+  methods: {
+    onClick(val) {
+      this.value1 = val
+    }
+  },
 }
 </script>
 
-<style>
-.index {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "taro-ui-vue3/dist/style/components/accordion.scss";
+@import "taro-ui-vue3/dist/style/components/icon.scss";
+
 </style>
